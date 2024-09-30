44f6409a4a4a89f4eeca6ee632114473ef3cd3f9
sirzes02 <sirzes02@gmail.com>
2024-09-29 23:00:47 -0500

    Initial Commit

---
+++ Instagram-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram-clone
+//
+//  Created by Santiago Varela on 29/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
